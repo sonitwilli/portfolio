6fec4742b48a3b243459e342a446c97b6c4f1580
Son Bui <builongkyson@gmail.com>
2016-10-18 14:57:35 +0700

    update about

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,9 @@ class HomeController extends Controller
         $header = Article::where('slug','header')->first();
         $portfolio_menu = Menus::where('slug','projects')->first();
         $portfolio = Category::where('menu_id',$portfolio_menu->id)->where('publish',1)->orderby('order_by')->with('block')->get();
+        $about_menu = Menus::where('slug','about')->where('parent_id',0)->first();
+        $about = Article::where('featured',0)->where('publish',1)->where('category_id',$about_menu->id)->orderby('order_by')->get();
+        $about_featured = Article::where('featured',1)->where('publish',1)->where('category_id',$about_menu->id)->orderby('order_by')->get();
         if(isset($home_menu)){
             $banner = Banner::where('category_id',$home_menu->id)->get();
         }else{
@@ -35,7 +38,9 @@ class HomeController extends Controller
             'web_title'     => $setting->title,
             'banner'        => $banner,
             'header'        => $header,
-            'portfolio'     => $portfolio
+            'portfolio'     => $portfolio,
+            'about'         => $about,
+            'about_featured'=> $about_featured
         ]) ;
     }
 
--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -10,47 +10,26 @@
         <div class="row">
             <div class="col-lg-5 col-lg-offset-1">
                 <table class="table">
-                    <tr>
-                        <td><i>Full name:</i></td>
-                        <td>Bùi Long Kỳ Sơn</td>
-                    </tr>
-                    <tr>
-                        <td><i>Gender:</i></td>
-                        <td>Male</td>
-                    </tr>
-                    <tr>
-                        <td><i>Date of Birth:</i></td>
-                        <td>20/03/1991</td>
-                    </tr>
-                    <tr>
-                        <td><i>Marital Status:</i></td>
-                        <td>Married</td>
-                    </tr>
-                    <tr>
-                        <td><i>Phone Number:</i></td>
-                        <td>0909326508</td>
-                    </tr>
-                    <tr>
-                        <td><i>Email:</i></td>
-                        <td>demondragon0101@gmail.com</td>
-                    </tr>
+                    @if(count($about) > 0)
+                        @foreach($about as $item)
+                            <tr>
+                                <td><i>{{ $item->name }}:</i></td>
+                                <td>{!! $item->content !!}</td>
+                            </tr>
+                        @endforeach
+                    @endif
                 </table>
             </div>
             <div class="col-lg-6">
                 <table class="table">
-                    <tr>
-                        <td><i>Graduate:</i></td>
-                        <td class="text-justify">
-                            University of Information Technology - Vietnam National University, Ho Chi Minh City.
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><i>Experience:</i></td>
-                        <td class="text-justify">
-                            Over 2 year website development by PHP.<br>
-                            Proficient HTML5, CSS3, javascript, jQuery, Bootstrap, Responsive, PHP, Laravel, CodeIgniter, Wordpress...
-                        </td>
-                    </tr>
+                    @if(count($about_featured) > 0)
+                        @foreach($about_featured as $item)
+                            <tr>
+                                <td><i>{{ $item->name }}:</i></td>
+                                <td class="text-justify">{!! $item->content !!}</td>
+                            </tr>
+                        @endforeach
+                    @endif
                 </table>
             </div>
         </div>
